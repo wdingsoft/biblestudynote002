edb54bc2df2390dcc423fb8542c1bcbcb2e205ce
System Administrator <wdingbox@gmail.com>
2020-12-30 20:20:51 -0500

    svr update ../../../../bible_study_notes/usrs/wdingsoft/biblestudynote002/account/dat/localStorage_json.js saved.

--- account/dat/localStorage_json.js
+++ account/dat/localStorage_json.js
@@ -1,8 +1,8 @@
 var localStorage = {
     "FontSize": "16",
     "repositories": "[{\"repository\":\"https://github.com/wdingsoft/biblestudynote002.git\",\"passcode\":\"3edcFDSA\"},{\"repository\":\"\",\"passcode\":\"\"}]",
-    "#MemoryVerse": "[\"Pro16:9\"]",
+    "#MemoryVerse": "[\"Pro8:22\",\"Pro16:9\"]",
     "test": "1,2",
-    "#RecentMarks": "[\"Pro16:9\"]",
+    "#RecentMarks": "[\"Pro8:22\",\"Pro16:9\"]",
     "#RecentBooks": "[\"Pro1:1\"]"
 }
